d6288dcde1d6d9117d6781185b866ec904a9e7e6
Shinya <hhhhhhyyyyyyy@nike.eonet.nejp>
2026-01-07 19:39:01 +0900

    Update design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // あなたのサイトのURL
-  site: 'https://shinya-u.github.io',
-  // リポジトリ名。これを入れないと画像やCSSが読み込めません
-  base: '/lastandgaba',
+  // base: '/lastandgaba',  ← 消す
+  // 他の設定そのまま
 });
