5e0d9363cf169013a3f5a05b43599dedf47e7659
Shinya <hhhhhhyyyyyyy@nike.eonet.nejp>
2026-01-07 14:53:52 +0900

    Update design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // あなたのサイトのURL
+  site: 'https://shinya-u.github.io',
+  // リポジトリ名。これを入れないと画像やCSSが読み込めません
+  base: '/lastandgaba',
+});
